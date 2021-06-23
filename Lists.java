5ed6bbbd9ecb784abdbd499472c5c192050193f4
YuktiGit <86337915+YuktiGit@users.noreply.github.com>
2021-06-23 10:57:54 +0530

    Add files via upload

---
+++ Lists.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Lists {
+    public static void main(String[] args){
+       System.out.println("List of Productive activities : ");
+       System.out.println("studying\ncoding\nreading\nwatchingnews\nexercising\nonlinecourses\nmeditation\nlearningnewskills\nblogging\ngardening\ncooking ");
+       
+       System.out.println("Lists of Non-Productive activities : ");
+       System.out.println("videogames\nchatting\nsocialmedia\nTV\nPUBG\neating\nsleeping\nlisteningmusic\nonlineseries ");
+    }   
+}
